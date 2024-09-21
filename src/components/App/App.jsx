8c024775420b957d4c9c633b510e32b337b15b9b
grizeus <zaytsewww@gmail.com>
2024-09-21 15:59:22 +0300

    .

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,38 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fetchWithTopic } from "../../api/fetch-api.js";
 import ArticleList from "../ArticleList/ArticleList.jsx";
+import Error from "../Error/Error.jsx";
+import Loader from "../Loader/Loader.jsx";
 import "./App.css";
+import SearchForm from "../SearchForm/SearchForm.jsx";
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    (async () => {
+  
+   const handleSearch = async (topic) => {
       try {
         setArticles([]);
         setError(false);
         setLoading(true);
 
-        const res = await fetchWithTopic("react");
-        setArticles(res);
+        const hits = await fetchWithTopic(topic);
+        setArticles(hits);
       } catch (err) {
         setError(true);
         console.error(err);
       } finally {
         setLoading(false);
       }
-    })();
-  }, []);
+  };
   return (
     <div>
       <h1>Latest articles</h1>
-      {loading && <p>Loading data, please wait...</p>}
-      {error && <p>Something went wrong, please try again later.</p>}
+      <SearchForm onSearch={handleSearch} />
+      {loading && <Loader />}
+      {error && <Error />}
       {articles.length > 0 && <ArticleList articles={articles} />}
     </div>
   );
--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,12 +1,10 @@
+import Article from "../Article/Article";
+
 const ArticleList = ({ articles }) => {
   return (
     <ul>
       {articles.map(({ objectID, title, url }) => (
-        <li key={objectID}>
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            {title}
-          </a>
-        </li>
+        <Article key={objectID} objectID={objectID} title={title} url={url} />
       ))}
     </ul>
   );
